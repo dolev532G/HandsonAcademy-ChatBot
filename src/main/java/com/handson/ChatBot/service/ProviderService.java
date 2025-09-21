657b4435968b4cf905582a033b5aa60d717187e1
Dolev Ben Shushan <dolev532@gmail.com>
2025-09-21 15:13:57 +0300

    with search call

--- src/main/java/com/handson/ChatBot/service/ProviderService.java
+++ src/main/java/com/handson/ChatBot/service/ProviderService.java
@@ -1,7 +1,12 @@
 package com.handson.ChatBot.service;
 
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class ProviderService {
@@ -9,4 +14,30 @@ public class ProviderService {
     public String searchProducts(String keyword) {
         return "Searched for:" + keyword;
     }
+
+    private String getMoviesHtml(String keyword) throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://www.imdb.com/find/?q=" + keyword)
+                .method("GET", null)
+                .addHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+                        "AppleWebKit/537.36 (KHTML, like Gecko) " +
+                        "Chrome/140.0.0.0 Safari/537.36")
+                .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8")
+                .addHeader("Accept-Language", "en-US,en;q=0.5")
+                .addHeader("Connection", "keep-alive")
+                .addHeader("Upgrade-Insecure-Requests", "1")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        if (!response.isSuccessful()) {
+            throw new IOException("Unexpected code " + response);
+        }
+
+        assert response.body() != null;
+        return response.body().string();
+    }
+
+
 }
